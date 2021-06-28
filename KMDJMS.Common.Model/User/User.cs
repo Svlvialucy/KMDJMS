db8e1b21a4764f9561951796d8918e760af7e461
Sylvialucy <sylvia.qian@foxmail.com>
2021-06-28 08:06:30 +0000

    init repo

---
+++ KMDJMS.Common.Model/User/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace KMDJMS.Common.Model.User
+{
+    [Table("KM_User")]
+    public class User
+    {
+        [Key]
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 常用电话
+        /// </summary>
+        public string CommonPhone { get; set; }
+
+        /// <summary>
+        /// 备用电话
+        /// </summary>
+        public string SparePhone { get; set; }
+
+        public UserRoleEnum 
+    }
+}
---
+++ KMDJMS.Common.Repository/DbContext/GeneralDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMDJMS.Common.Repository.DbContext
+{
+    public class GeneralDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        private const string DbConnectionStringAppSetting = "DbConnectionString_KMDJMS";
+
+    }
+}
